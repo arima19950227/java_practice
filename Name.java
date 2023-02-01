fc5173d53337ab13a2ab052c270e5072646bbc2d
arima19950227 <arima.macbook@gmail.com>
2023-02-01 16:41:48 +0000

    条件分岐練習

--- Name.java
+++ Name.java
@@ -4,7 +4,18 @@ class Name {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("名前:");
-    String firstname=scanner.nextLine();
-    System.out.println("名前は"+firstname+"です");
+    String firstname=scanner.next();
+    System.out.print("名字:");
+    String lastName=scanner.next();
+    System.out.print("年齢:");
+    int age=scanner.nextInt();
+    String name=firstname+""+lastName;
+    System.out.println("名前は"+name+"です");
+    System.out.println("年齢は"+age+"です");
+    if(age>=20){
+      System.out.println("成年者です");
+    }else{
+      System.out.println("未成年者です");
+    }
   }
 }
