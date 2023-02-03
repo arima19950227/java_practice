fba2e26eb87e45643d6928a1f48ec11577318c17
arima19950227 <arima.macbook@gmail.com>
2023-02-03 15:58:15 +0000

    クラス分け

--- Weight.java
+++ Weight.java
@@ -3,19 +3,28 @@ import java.util.Scanner;
 class Weight {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    
+    
     System.out.print("名前:");
-    String firstname=scanner.next();
+    String firstName=scanner.next();
+    
     System.out.print("名字:");
     String lastName=scanner.next();
+    
     System.out.print("年齢:");
     int age=scanner.nextInt();
+    
     System.out.print("身長(m):");
-    Double height=scanner.nextDouble();
+    double height=scanner.nextDouble();
+    
     System.out.print("体重(kg):");
-    Double weight=scanner.nextDouble();
-
-    String name=firstname+""+lastName;
-    System.out.println("名前は"+name+"です");
+    double weight=scanner.nextDouble();
+   
+   printData(firstName,lastName,age,height,weight);
+ }
+  
+  public static void printData(String firstName, String lastName, int age, double height, double weight){
+    System.out.println("名前は"+fullName(firstName,lastName)+"です");
     System.out.println("年齢は"+age+"歳です");
     if(age>=20){
       System.out.println("成年者です");
@@ -24,5 +33,12 @@ class Weight {
     }
     System.out.println("身長は"+height+"cmです");
     System.out.println("体重は"+weight+"kgです");
+    
+  }
+    
+public static String fullName(String firstName, String lastName){
+  return firstName+""+lastName;
   }
 }
+
+  
