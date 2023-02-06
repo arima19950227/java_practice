ced8d225f864c418693fa3da50ce2591f739bf39
arima19950227 <arima.macbook@gmail.com>
2023-02-06 18:16:00 +0000

    最高年齢出力

--- Bmi.java
+++ Bmi.java
@@ -6,6 +6,7 @@ class Bmi {
 
     System.out.print("何人分の情報を入力しますか？");
     int n=scanner.nextInt();
+    int maxAge=0;
 
     for(int i=0; i<n; i++){
 
@@ -19,6 +20,10 @@ class Bmi {
 
     System.out.print("年齢:");
     int age=scanner.nextInt();
+    if(age>maxAge){
+      maxAge=age;
+    }
+    
 
     System.out.print("身長(m):");
     double height=scanner.nextDouble();
@@ -29,5 +34,8 @@ class Bmi {
     Person.printData(firstName, lastName, age, height, weight);
 
     }
+    
+    System.out.println("最高年齢は"+maxAge+"です");
+    
   }
 }
