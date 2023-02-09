954bb767cec4540ba005c3411641ec1155aa57d0
arima19950227 <arima.macbook@gmail.com>
2023-02-09 18:10:35 +0000

    javaクラスフィールド学習

--- Const.java
+++ Const.java
@@ -3,5 +3,13 @@ class Const {
    Test person1 = new Test("有馬", "大地", 27, 1.8, 78.0);
    person1.printData();
 
+  Test person2 = new Test("有馬", "緑咲", 22, 1.65, 54.0);
+  person2.printData();
+
+  Test person3 = new Test("有馬", "遙清", 0, 0.5, 0.5);
+  person3.printData();
+
+ System.out.println("合計人数は"+Test.count+"人です");
+
   }
 }
--- Test.java
+++ Test.java
@@ -1,34 +1,39 @@
 class Test {
+
+  public static int count=0;
   public String firstName;
   public String lastName;
   public int age;
   public double height;
   public double weight;
-  
+
   Test(String firstName,String lastName,int age,double height,double weight){
+
+    Test.count++;
+
     this.firstName=firstName;
     this.lastName=lastName;
     this.age=age;
     this.height=height;
     this.weight=weight;
   }
-  
+
   public String fullName(){
     return this.firstName+this.lastName;
   }
-  
+
   public double bmi(){
     return this.weight/this.height/this.height;
   }
-  
+
   public void printData(){
     String fullName=this.fullName();
     double bmi=this.bmi();
-    
+
     System.out.println("私の名前は"+fullName+"です");
     System.out.println("年齢は"+this.age+"歳です");
     System.out.println("BMIは"+Math.round(bmi)+"です");
-    
+
   }
 
 }
