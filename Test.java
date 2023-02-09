d4ebee6eacf35fa4eb876126f421b352c5fdd9f7
arima19950227 <arima.macbook@gmail.com>
2023-02-09 18:16:39 +0000

    コメント入れ

--- Test.java
+++ Test.java
@@ -1,6 +1,9 @@
 class Test {
 
+  //クラスフィールド↓,staticをつける
   public static int count=0;
+
+  //インスタンスフィールド↓
   public String firstName;
   public String lastName;
   public int age;
@@ -9,8 +12,10 @@ class Test {
 
   Test(String firstName,String lastName,int age,double height,double weight){
 
+    //初期値0の値にインクリメントしていく
     Test.count++;
 
+  //constでの引数をインスタンスフィールドの値に代入していく
     this.firstName=firstName;
     this.lastName=lastName;
     this.age=age;
@@ -26,12 +31,16 @@ class Test {
     return this.weight/this.height/this.height;
   }
 
+  //出力するprintDataメソッド
   public void printData(){
     String fullName=this.fullName();
     double bmi=this.bmi();
 
     System.out.println("私の名前は"+fullName+"です");
+
     System.out.println("年齢は"+this.age+"歳です");
+
+    //Math.round(引数)で四捨五入することができる
     System.out.println("BMIは"+Math.round(bmi)+"です");
 
   }
