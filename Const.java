80f9f9585dfd0e3279ec7254961c064313d43ff3
arima19950227 <arima.macbook@gmail.com>
2023-02-10 15:42:15 +0000

    インスタンスフィールド,コンストラクタのオーバーロード&呼び出し,条件分岐

--- Const.java
+++ Const.java
@@ -3,7 +3,7 @@ class Const {
    Test person1 = new Test("有馬", "大地", 27, 1.8, 78.0);
    person1.printData();
 
-  Test person2 = new Test("有馬", "緑咲", 22, 1.65, 54.0);
+  Test person2 = new Test("有馬","マイケル", "緑咲", 22, 1.65, 54.0);
   person2.printData();
 
   Test person3 = new Test("有馬", "遙清", 0, 0.5, 0.5);
--- Test.java
+++ Test.java
@@ -5,6 +5,7 @@ class Test {
 
   //インスタンスフィールド↓
   public String firstName;
+  public String middleName;
   public String lastName;
   public int age;
   public double height;
@@ -23,8 +24,18 @@ class Test {
     this.weight=weight;
   }
 
+  Test(String firstName,String middleName,String lastName,int age,double height,double weight){
+    this(firstName,lastName,age,height,weight);
+    this.middleName=middleName;
+  }
+
+
   public String fullName(){
-    return this.firstName+this.lastName;
+    if(this.middleName==null){
+      return this.firstName+""+this.lastName;
+    }else{
+      return this.firstName+this.middleName+this.lastName;
+    }
   }
 
   public double bmi(){
